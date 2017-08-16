ee7c817c430a18c1ba784f42469e01418ad5c303
Adriel P. de Guzman <adeguzman@stratpoint.com>
2017-08-16 15:26:51 +0800

    Create base component for vuelma-form
    - Create basic input-field component as well

---
+++ src/components/VuelmaForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form
+    class="VuelmaForm"
+    @submit.prevent="$emit('submit')"
+  >
+    <template v-for="field in fields">
+      <input-field
+        v-model="formObject[fied.name]"
+        :type="field.type"
+        :placeholder="field.placeholder"
+        :key="field.name"
+      ></input-field>
+    </template>
+  </form>
+</template>
+
+<script>
+import InputField from './VuelmaForm/Input';
+
+export default {
+  name: 'vuelma-form',
+  components: {
+    InputField,
+  },
+  props: {
+    /**
+     * The fields that will be rendered by the component.
+     */
+    fields: {
+      type: Array,
+      required: true,
+    },
+
+    /**
+     * The object containing the data associated to the form.
+     */
+    formObject: {
+      type: Object,
+      required: true,
+    },
+
+    /**
+     * The object containing form (validation) errors.
+     */
+    errors: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
---
+++ src/components/VuelmaForm/Input.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="TextField field">
+    <div class="control">
+      <input
+        type="text"
+        class="input"
+        :placeholder="placeholder"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'text-field',
+  props: {
+    /**
+     * The bound data from the form component.
+     */
+    value: {
+      type: String,
+      reqiured: true,
+    },
+
+    /**
+     * The placeholder text if the field is empty.
+     */
+    placeholder: {
+      type: String,
+    },
+  },
+};
+</script>
