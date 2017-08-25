110afc715e60e7825eca84adbd5c35c7a81f1540
Adriel P. de Guzman <adeguzman@stratpoint.com>
2017-08-25 10:54:54 +0800

    Add BEM for FormControl

--- src/components/FormControl.vue
+++ src/components/FormControl.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="field"
+    class="VuelmaForm__FormControl field"
     :class="{
       'is-horizontal': horizontal,
       'has-addons': hasAddonsLeft || hasAddonsRight,
