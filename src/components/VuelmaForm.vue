256182d932a33a475aff4a0a7288dbaa41d6a768
Adriel P. de Guzman <adeguzman@stratpoint.com>
2017-09-14 09:55:38 +0800

    Add default prop for formErrors

--- src/components/VuelmaForm.vue
+++ src/components/VuelmaForm.vue
@@ -47,7 +47,10 @@ export default {
     /**
      * The object containing form (validation) errors.
      */
-    formErrors: Object,
+    formErrors: {
+      type: Object,
+      default: () => ({}),
+    },
   },
 };
 </script>
