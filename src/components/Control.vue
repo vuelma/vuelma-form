ca7db6671cee6a1987ab2693bd8c8d64bf169334
Adriel P. de Guzman <adeguzman@stratpoint.com>
2017-10-01 00:09:41 +0800

    Create HoC component for form controls

---
+++ src/components/Control.vue
@@ -0,0 +1,40 @@
+<script>
+import bus from '../utils/bus';
+
+export default {
+  name: 'control',
+  props: {
+    /**
+     * The bound data from the form component.
+     */
+    value: {
+      type: [String, Number, Array, Boolean, File],
+      reqiured: true,
+    },
+
+    /**
+     * The input/field type of the component.
+     */
+    type: {
+      type: String,
+      default: 'text',
+    },
+
+    /**
+     * The unique name of the field.
+     */
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    input(event) {
+      bus.$emit('update:model', {
+        name: event.target.name,
+        value: event.target.value,
+      });
+    },
+  },
+};
+</script>
