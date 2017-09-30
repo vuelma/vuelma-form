9778f4f22e3e4caf54be25643f82f8e5d66fa9ea
Adriel P. de Guzman <adeguzman@stratpoint.com>
2017-10-01 00:10:38 +0800

    Create initial implementation for Input-type control

---
+++ src/components/Input.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    class="VuelmaForm__input input"
+    v-bind="$props"
+    :id="name"
+    @input="input"
+  >
+</template>
+
+<script>
+import Control from './Control';
+
+export default {
+  name: 'input-control',
+  extends: {
+    props: {
+      ...Control.props,
+    },
+    methods: {
+      ...Control.methods,
+    },
+  },
+};
+</script>
