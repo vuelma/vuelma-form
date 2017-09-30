9d5b4c784509269801cb5c27721ea1f4fb0da89d
Adriel P. de Guzman <adeguzman@stratpoint.com>
2017-10-01 00:09:58 +0800

    Extract field as component in preparation for horizontal forms

---
+++ src/components/Field.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="VuelmaForm__field field">
+    <component
+      :is="control"
+      v-bind="field"
+      :value="value"
+      :errors="errors"
+    ></component>
+  </div>
+</template>
+
+<script>
+import InputControl from './Input';
+
+export default {
+  name: 'vuelma-field',
+  components: {
+    InputControl,
+  },
+  props: ['field', 'value', 'errors'],
+  computed: {
+    control() {
+      switch (this.field.type) {
+        default:
+          return 'input-control';
+      }
+    },
+  },
+};
+</script>
