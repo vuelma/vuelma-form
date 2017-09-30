82c706e888ef04d08b4d462a06820d08d88c25f2
Adriel P. de Guzman <adeguzman@stratpoint.com>
2017-10-01 01:03:11 +0800

    Add support for Select and Option

---
+++ src/components/Option.vue
@@ -0,0 +1,31 @@
+<template>
+  <option
+    v-bind="$props"
+  ></option>
+</template>
+
+<script>
+export default {
+  name: 'option-control',
+  props: {
+    /**
+     * The value associated with the option.
+     */
+    value: {
+      type: [String, Boolean, Number],
+      required: true,
+    },
+
+    /**
+     * The text/label of the option.
+     */
+    label: String,
+
+    /**
+     * Native HTML attributes for option.
+     */
+    selected: Boolean,
+    disabled: Boolean,
+  },
+};
+</script>
---
+++ src/components/Select.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="select">
+    <select
+      v-bind="$props"
+      :id="name"
+      @input="input"
+    >
+      <option-control
+        disabled
+        selected
+        :label="placeholder"
+        value=""
+      ></option-control>
+      <option-control
+        v-for="option in options"
+        :key="option.value"
+        v-bind="option"
+      ></option-control>
+    </select>
+  </div>
+  </div>
+</template>
+
+<script>
+import Control from './Control';
+import OptionControl from './Option';
+
+export default {
+  name: 'select-control',
+  extends: Control,
+  components: {
+    OptionControl,
+  },
+  props: {
+    /**
+     * The options for the select control.
+     */
+    options: {
+      type: Array,
+      required: true,
+    },
+
+    /**
+     * The placeholder label for the default option.
+     */
+    placeholder: String,
+  },
+};
+</script>
